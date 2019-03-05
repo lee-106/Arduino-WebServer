e0c09411ea7c50dd5fe1ed2b253bfd6333d873cc
Christoph Kitane <christoph_kitane@dlsu.edu.ph>
2019-03-05 12:39:21 +0800

    took away Panel.DefaultButton in aspx due to errors

--- WebApplication/ex2Interface.aspx.designer.cs
+++ WebApplication/ex2Interface.aspx.designer.cs
@@ -58,6 +58,15 @@ namespace WebApplication {
         protected global::System.Web.UI.WebControls.Label update_Lbl;
         
         /// <summary>
+        /// Panel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel1;
+        
+        /// <summary>
         /// Intensity_label control.
         /// </summary>
         /// <remarks>
