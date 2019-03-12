d5101e3e700182c54bd5fc18adf725f6ca44feda
Maverick Rivera <maverick_rivera@dlsu.edu.ph>
2019-03-12 08:10:12 +0700

    edited ex4 interface

--- WebApplication/ex4Interface.aspx.cs
+++ WebApplication/ex4Interface.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO.Ports;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -8,16 +9,66 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication
 {
+    
     public partial class ex4Interface : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        
+
+
+        SerialPort ArduinoPort1,ArduinoPort2;
+
+      
+
+        protected void Page_Load(object sender, EventArgs asd)
         {
+           
+            if (Page.IsPostBack)
+            {
+
+                 ArduinoPort1 = new SerialPort();
+                ArduinoPort2 = new SerialPort();
+                if ((Port_TB1.Text.ToLower().Contains("com") && Port_TB2.Text.ToLower().Contains("com") )|| (!Port_TB1.Text.Equals(null) && !Port_TB2.Text.Equals(null)))
+                {
+                    ArduinoPort1.PortName = Port_TB1.Text;
+                    
+                    ArduinoPort2.PortName = Port_TB2.Text;
+                    update_Lbl.Text = "Connection Established: " + ArduinoPort1.PortName + "," + ArduinoPort1.BaudRate +"\n\r"
+                        + "Connection Established: " + ArduinoPort2.PortName + "," + ArduinoPort2.BaudRate;
+
+                }
+                else
+                {
+                    update_Lbl.Text = "Please fill in the textbox above with the proper port name (i.e. COM1)";
+                }
+
+
+            }
 
         }
         
         protected void backBtn_Clicked(object sender, EventArgs e)
         {
             Response.Redirect("ex4Form.aspx");
+        }
+
+        protected void tyrButton_Click(object sender, EventArgs e)
+        {
+            ArduinoPort1.Open();
+            tryLabel.Text = ArduinoPort1.ReadLine();
+            ArduinoPort1.Close();
+
+        }
+        protected void Port_TB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected void Connect_Btn_Click(object sender, EventArgs e)
+        {
+
+        }
+        protected void Evaluate()
+        {
+
         }
     }
 }
